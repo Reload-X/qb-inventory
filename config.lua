b3c9d2602b031abf43dfe140f253cdd7c2b67717
Reload <167034229+Reload-X@users.noreply.github.com>
2024-04-15 08:01:48 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,12 @@
 Config = {}
 
-Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
-
 Config.MaxInventoryWeight = 120000 -- Max weight a player can carry (default 120kg, written in grams)
 Config.MaxInventorySlots = 41 -- Max inventory slots for a player
 
 Config.CleanupDropTime = 15 * 60 -- How many seconds it takes for drops to be untouched before being deleted
 Config.MaxDropViewDistance = 12.5 -- The distance in GTA Units that a drop can be seen
-Config.UseItemDrop = true -- This will enable item object to spawn on drops instead of markers
+Config.UseItemDrop = true -- This will enable item object to spawn on drops instead of markers // RECOMMEND TO LEAVE TRUE
+Config.TargetSystem = 'qb-target' -- choose between qb-target/interact
 Config.ItemDropObject = `sf_prop_sf_backpack_01a` -- if Config.UseItemDrop is true, this will be the prop that spawns for the item
 Config.Waittime = 0 -- Time to open inventory
 
@@ -21,6 +20,17 @@ Config.VendingObjects = {
     "prop_vend_soda_01",
     "prop_vend_soda_02",
     "prop_vend_water_01"
+}
+
+Config.toolBoxModels = {
+        `prop_toolchest_05`,
+        `prop_tool_bench02_ld`,
+        `prop_tool_bench02`,
+        `prop_toolchest_02`,
+        `prop_toolchest_03`,
+        `prop_toolchest_03_l2`,
+        `prop_toolchest_05`,
+        `prop_toolchest_04`,
 }
 
 Config.CraftingObject = `prop_toolchest_05` -- Only needed if not using target | Line 928 to change Target Models
@@ -56,7 +66,7 @@ Config.CraftingItems = {
         type = "item",
         slot = 1,
         threshold = 0,
-        points = 1,
+        points = 0,
     },
     [2] = {
         name = "screwdriverset",
@@ -215,122 +225,96 @@ Config.CraftingItems = {
     },
 }
 
-Config.AttachmentCraftingLocation = vector3(88.91, 3743.88, 40.77) -- Only needed if not using target
-
 Config.AttachmentCrafting = {
-    [1] = {
-        name = "pistol_extendedclip",
+    {
+        name = 'pistol_extendedclip',
         amount = 50,
-        info = {},
-        costs = {
-            ["metalscrap"] = 140,
-            ["steel"] = 250,
-            ["rubber"] = 60,
-        },
-        type = "item",
-        slot = 1,
         threshold = 0,
         points = 1,
-    },
-    [2] = {
-        name = "pistol_suppressor",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 165,
-            ["steel"] = 285,
-            ["rubber"] = 75,
+            ['metalscrap'] = 140,
+            ['steel'] = 250,
+            ['rubber'] = 60,
         },
-        type = "item",
-        slot = 2,
+    },
+    {
+        name = 'pistol_suppressor',
+        amount = 50,
         threshold = 10,
         points = 2,
-    },
-    [3] = {
-        name = "smg_extendedclip",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 190,
-            ["steel"] = 305,
-            ["rubber"] = 85,
+            ['metalscrap'] = 165,
+            ['steel'] = 285,
+            ['rubber'] = 75,
         },
-        type = "item",
-        slot = 3,
+    },
+    {
+        name = 'smg_extendedclip',
+        amount = 50,
         threshold = 25,
         points = 3,
-    },
-    [4] = {
-        name = "microsmg_extendedclip",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 205,
-            ["steel"] = 340,
-            ["rubber"] = 110,
+            ['metalscrap'] = 190,
+            ['steel'] = 305,
+            ['rubber'] = 85,
         },
-        type = "item",
-        slot = 4,
+    },
+    {
+        name = 'microsmg_extendedclip',
+        amount = 50,
         threshold = 50,
         points = 4,
-    },
-    [5] = {
-        name = "smg_drum",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 230,
-            ["steel"] = 365,
-            ["rubber"] = 130,
+            ['metalscrap'] = 205,
+            ['steel'] = 340,
+            ['rubber'] = 110,
         },
-        type = "item",
-        slot = 5,
+    },
+    {
+        name = 'smg_drum',
+        amount = 50,
         threshold = 75,
         points = 5,
-    },
-    [6] = {
-        name = "smg_scope",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 255,
-            ["steel"] = 390,
-            ["rubber"] = 145,
+            ['metalscrap'] = 230,
+            ['steel'] = 365,
+            ['rubber'] = 130,
         },
-        type = "item",
-        slot = 6,
+    },
+    {
+        name = 'smg_scope',
+        amount = 50,
         threshold = 100,
         points = 6,
-    },
-    [7] = {
-        name = "assaultrifle_extendedclip",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 270,
-            ["steel"] = 435,
-            ["rubber"] = 155,
-            ["smg_extendedclip"] = 1,
+            ['metalscrap'] = 255,
+            ['steel'] = 390,
+            ['rubber'] = 145,
         },
-        type = "item",
-        slot = 7,
+    },
+    {
+        name = 'assaultrifle_extendedclip',
+        amount = 50,
         threshold = 150,
         points = 7,
-    },
-    [8] = {
-        name = "assaultrifle_drum",
-        amount = 50,
-        info = {},
         costs = {
-            ["metalscrap"] = 300,
-            ["steel"] = 469,
-            ["rubber"] = 170,
-            ["smg_extendedclip"] = 2,
+            ['metalscrap'] = 270,
+            ['steel'] = 435,
+            ['rubber'] = 155,
+            ['smg_extendedclip'] = 1,
         },
-        type = "item",
-        slot = 8,
+    },
+    {
+        name = 'assaultrifle_drum',
+        amount = 50,
         threshold = 200,
         points = 8,
+        costs = {
+            ['metalscrap'] = 300,
+            ['steel'] = 469,
+            ['rubber'] = 170,
+            ['smg_extendedclip'] = 2,
+        },
     },
 }
 
