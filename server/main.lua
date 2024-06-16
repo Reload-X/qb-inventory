d58a0389cdb0b4799a9aacd03a266fc1fcc9e1a9
Reload <167034229+Reload-X@users.noreply.github.com>
2024-06-16 02:04:38 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -218,7 +218,7 @@ local function AddItem(source, item, amount, slot, info, created)
 			end
 		end
 	elseif not Player.Offline then
-		QBInventoryNotify(source, Config.Lang["benInventoryTooFull"], "error")
+		QBInventoryNotify(source, Config.Lang["InventoryTooFull"], "error")
 	end
 	return false
 end
