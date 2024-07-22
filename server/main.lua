d2f6151432ca2169f4972da3b32cf687406222fb
Reload <167034229+Reload-X@users.noreply.github.com>
2024-07-22 20:47:24 +0100

    Update main.lua
    * Fix issue with items going missing in stash
    
    * Fixes a bug where placing an item in a stash and then closing and 
       reopening it would make the item go missing

--- server/main.lua
+++ server/main.lua
@@ -1031,6 +1031,7 @@ local function OpenInventory(name, id, other, origin)
 					Stashes[id].isOpen = src
 					Stashes[id].label = secondInv.label
 				end
+				TriggerClientEvent('inventory:client:SetCurrentStash', src, id)
 			end
 		elseif name == "trunk" then
 			if Trunks[id] then
